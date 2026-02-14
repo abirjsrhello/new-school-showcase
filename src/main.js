0634cbdc33e4f374c7863844de5628ed35b21458
abir das <saasproject21@gmail.com>
2026-02-14 17:55:16 +0600

    Create main.js with basic starter code

---
+++ src/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Basic JavaScript starter code for a showcase application
+
+const showcaseApp = () => {
+    console.log('Welcome to the Showcase Application!');
+    // Initialize application logic here
+};
+
+showcaseApp();
